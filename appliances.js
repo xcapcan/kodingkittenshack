1a10de7768be40a57f4506eea310c813b9021063
lun-ac <68301634+lun-ac@users.noreply.github.com>
2020-07-26 01:40:57 -0700

    Add files via upload

---
+++ appliances.js
@@ -0,0 +1,98 @@
+function MiniFridge() {
+    var T = document.getElementById("MiniFridge"),
+        displayValue = "";
+    if (T.style.display == "")
+        displayValue = "none";
+
+    T.style.display = displayValue;
+}
+
+function Micro() {
+    var M = document.getElementById("Micro"),
+        displayValue = "";
+    if (M.style.display == "")
+        displayValue = "none";
+
+    M.style.display = displayValue;
+}
+
+function Rice() {
+    var R = document.getElementById("Rice"),
+        displayValue = "";
+    if (R.style.display == "")
+        displayValue = "none";
+
+    R.style.display = displayValue;
+}
+
+function Blender() {
+    var B = document.getElementById("Blender"),
+        displayValue = "";
+    if (B.style.display == "")
+        displayValue = "none";
+
+    B.style.display = displayValue;
+}
+
+function Toaster() {
+    var To = document.getElementById("Toaster"),
+        displayValue = "";
+    if (To.style.display == "")
+        displayValue = "none";
+
+    To.style.display = displayValue;
+}
+
+function Water() {
+    var W = document.getElementById("Water"),
+        displayValue = "";
+    if (W.style.display == "")
+        displayValue = "none";
+
+    W.style.display = displayValue;
+}
+
+function Grill() {
+    var G = document.getElementById("Grill"),
+        displayValue = "";
+    if (G.style.display == "")
+        displayValue = "none";
+
+    G.style.display = displayValue;
+}
+
+function Coffee() {
+    var C = document.getElementById("Coffee"),
+        displayValue = "";
+    if (C.style.display == "")
+        displayValue = "none";
+
+    C.style.display = displayValue;
+}
+
+function Hotpot() {
+    var H = document.getElementById("Hotpot"),
+        displayValue = "";
+    if (H.style.display == "")
+        displayValue = "none";
+
+    H.style.display = displayValue;
+}
+
+function Ramen() {
+    var R = document.getElementById("Ramen"),
+        displayValue = "";
+    if (R.style.display == "")
+        displayValue = "none";
+
+    R.style.display = displayValue;
+}
+
+function Spiralizer() {
+    var S = document.getElementById("Spiralizer"),
+        displayValue = "";
+    if (S.style.display == "")
+        displayValue = "none";
+
+    S.style.display = displayValue;
+}
